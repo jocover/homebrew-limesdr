842b58f90e4ca8d1c3ba594a8431a2abb690040a
jocover <jiangwei0402@gmail.com>
2017-06-26 11:08:10 +0800

    change liquid-dap version 1.3.0

--- liquid-dsp.rb
+++ liquid-dsp.rb
@@ -4,7 +4,9 @@ class LiquidDsp < Formula
   desc "digital signal processing library for software-defined radios library"
   homepage "http://liquidsdr.org/"
   head "https://github.com/jgaeddert/liquid-dsp.git"
-  url "https://github.com/jgaeddert/liquid-dsp.git"
+  url "https://github.com/jgaeddert/liquid-dsp/archive/v1.3.0.tar.gz"
+  sha256 "b136343d644bc1441f7854f2d292bfa054e8d040c0b745879b205f6836dca0f0"
+  
   depends_on "fftw"
   depends_on "autoconf" => :build
   depends_on "automake" => :build
